b8a7f3efc3d1962edf6fc490aeccf77bd33b365c
Sang <sanglun2254@gmail.com>
2018-11-23 19:48:17 +0700

    index,detail

---
+++ ASPNETCore_Final/ASPNETCore_Final/Controllers/LoaiController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNETCore_Final.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCore_Final.Controllers
+{
+    public class LoaiController : Controller
+    {
+        private readonly ESHOPContext db;
+
+        public LoaiController(ESHOPContext ctx)
+        {
+            db = ctx;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DSLoai()
+        {
+            return View();
+        }
+    }
+}
