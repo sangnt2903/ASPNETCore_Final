f6a635bdcb1fc1be6c75b7628bf0ed8c53607ff9
Sang <sanglun2254@gmail.com>
2018-11-23 14:24:17 +0700

    layout

--- ASPNETCore_Final/ASPNETCore_Final/Startup.cs
+++ ASPNETCore_Final/ASPNETCore_Final/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASPNETCore_Final.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -31,7 +33,7 @@ namespace ASPNETCore_Final
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+            services.AddDbContext<ESHOPContext>(option => option.UseSqlServer(Configuration.GetConnectionString("Eshop")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
