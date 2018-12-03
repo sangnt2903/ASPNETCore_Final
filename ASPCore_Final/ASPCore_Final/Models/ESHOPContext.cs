d45fc847ae00c55fdb9f524aafe1d7f0f6fbaa05
Cao Duc Minh <minh21298@gmail.com>
2018-12-03 18:59:27 +0700

    Add kich hoat tai khoan

--- ASPCore_Final/ASPCore_Final/Models/ESHOPContext.cs
+++ ASPCore_Final/ASPCore_Final/Models/ESHOPContext.cs
@@ -34,7 +34,7 @@ namespace ASPCore_Final.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=THANHSANG\\SQLEXPRESS;Database=ESHOP;Integrated Security=True;");
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=ESHOP;Integrated Security=True;");
             }
         }
 
