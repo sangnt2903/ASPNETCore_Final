64c4627d1ca1aa2abf68fb0addbe0ab655cc37b1
Nguyen Hoang Anh Tu <nguyenhoanganhtu2406@gmail.com>
2018-12-18 22:14:35 +0700

    ajax phân trang

--- ASPCore_Final/ASPCore_Final/Controllers/HangHoaController.cs
+++ ASPCore_Final/ASPCore_Final/Controllers/HangHoaController.cs
@@ -15,22 +15,96 @@ namespace ASPCore_Final.Controllers
             db = ctx;
         }
 
-        public IActionResult Index(int page = 1, int pageSize = 6)
+        
+        public IActionResult Index(string loai="", bool? gioitinh = null, int page = 1, int pageSize = 6)
         {
             int starIndex = (page - 1) * pageSize;
-            List<HangHoa> hangHoas = db.HangHoa.Skip(starIndex).ToList();
+            List<HangHoa> hangHoas =  db.HangHoa.Skip(starIndex).ToList();
+            if (gioitinh != null)
+            {
+                hangHoas = db.HangHoa.Where(p => p.MaLoaiNavigation.GioiTinh == gioitinh).Skip(starIndex).ToList();
+            }
+            else if(loai != null && loai != "")
+            {
+                hangHoas = db.HangHoa.Where(p => p.MaLoai == loai).Skip(starIndex).ToList();
+            }
             int itemsize = hangHoas.Count < pageSize ? hangHoas.Count : pageSize;
-            List<HangHoa> res = hangHoas.Take(itemsize).ToList();
+            List<HangHoa> res = hangHoas.Take(itemsize).ToList();   
+            
             return View("Index",res);
         }
 
-        public IActionResult Index1(string loai="", int page = 1, int pageSize = 6)
+        public List<HangHoaAjaxModel> TaiThem(string loai = "", bool? gioitinh = null, int page = 2, int pageSize = 6)
         {
             int starIndex = (page - 1) * pageSize;
-            List<HangHoa> hangHoas = db.HangHoa.Where(p => p.MaLoai == loai).Skip(starIndex).ToList();
+            List<HangHoa> hangHoas = db.HangHoa.Skip(starIndex).ToList();
+            if (gioitinh != null)
+            {
+                hangHoas = db.HangHoa.Where(p => p.MaLoaiNavigation.GioiTinh == gioitinh).Skip(starIndex).ToList();
+            }
+            else if (loai != null && loai != "")
+            {
+                hangHoas = db.HangHoa.Where(p => p.MaLoai == loai).Skip(starIndex).ToList();
+            }
             int itemsize = hangHoas.Count < pageSize ? hangHoas.Count : pageSize;
-            List<HangHoa> res = hangHoas.Take(itemsize).ToList();
-            return View("Index",res);
+            List<HangHoa> resHH = hangHoas.Take(itemsize).ToList();
+            List<HangHoaAjaxModel> res = new List<HangHoaAjaxModel>();
+            foreach (var item in resHH)
+            {
+                List<SanPhamKho> sanPhamKhos = db.SanPhamKho.Where(p => p.MaHh == item.MaHh).ToList();
+                var soluong = sanPhamKhos.Sum(x => x.SoLuong);
+                string ghiChu = "";
+                if (soluong > 10)
+                {
+                    double gg = item.GiamGia * 100;
+                    ghiChu = "-" + gg + "%";
+                }
+                else if (soluong > 0)
+                {
+                    ghiChu = "Sắp hết hàng";
+                }
+                else
+                {
+                    ghiChu = "Tạm hết hàng";
+                }
+
+                List<YeuThich> yts = db.YeuThich.Where(p => p.MaHh == item.MaHh).ToList();
+                double diemyt = 0;
+                int diem = 5;
+                if (yts.Count > 0)
+                {
+                    double tongdiem = 0;
+                    foreach (var y in yts)
+                    {
+                        tongdiem = tongdiem + y.DiemDanhGia;
+                    }
+                    diemyt = tongdiem / yts.Count();
+                    diem = int.Parse(diemyt.ToString().Split(".")[0]);
+                    if (diem < 3)
+                    {
+                        diem = 3;
+                    }
+                }
+
+                double giaBan = item.DonGia * (1 - item.GiamGia);
+
+                HangHoaAjaxModel hhModel = new HangHoaAjaxModel
+                {
+                    MaHh = item.MaHh,
+                    TenHh = item.TenHh,
+                    Hinh = item.Hinh,
+                    GhiChu = ghiChu,
+                    Diem = diem,
+                    DonGia = item.DonGia,
+                    GiamGia = item.GiamGia,
+                    GiaBan = giaBan,
+                    MoTa = item.MoTa,
+                    MaLoai = item.MaLoai,
+                    MaNcc = item.MaNcc
+                };
+                res.Add(hhModel);
+            }
+            return res;
         }
 
         public IActionResult ChiTiet(int mahh)
