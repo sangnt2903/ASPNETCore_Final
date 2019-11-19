88306cf8a0562fbffbf181e4608b8f1fe22aa0ae
MSI\ADMIN <sanglun2254@gmail.com>
2019-11-20 00:07:41 +0700

    connectionstring

--- ASPCore_Final/ASPCore_Final/Models/ESHOPContext.cs
+++ ASPCore_Final/ASPCore_Final/Models/ESHOPContext.cs
@@ -43,7 +43,7 @@ namespace ASPCore_Final.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=MSI\\SQL_EXPRESS;Database=ESHOP;Integrated Security=True;");
+                optionsBuilder.UseSqlServer("Server=tcp:eshophcmue.database.windows.net,1433;Initial Catalog=ESHOP;Persist Security Info=False;User ID=EShop;Password=Hihi1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
